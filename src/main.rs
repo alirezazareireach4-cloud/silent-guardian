bcd5394671a6666595dd6e33ba0f6ca0c9b8fa48
Alireza Zarei <alirezazarei.reach4@gmail.com>
2026-07-08 10:31:27 +0330

    Configure application window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+
+
+use macroquad::prelude::*;
+
+
+fn window_conf()->Conf{
+    Conf{
+        window_title : "Silent Guardian".to_owned(),
+        window_height : 720,
+        window_width : 1280,
+        ..Default::default()
+    }
 }
+
+#[macroquad::main(window_conf)]
+async fn main(){
+    loop {
+        clear_background(Color::from_rgba(26,29,41,255));
+        next_frame().await;
+    }
+}
+
